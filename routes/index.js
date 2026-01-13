19aa168e220eed84f8a375d7ebba62d20d65935b
Josephlovelace <shawn.robbinhood@gmail.com>
2026-01-13 08:26:36 -0700

    Don't wait response of API

--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,12 @@ const axios = require("axios");
 const router = express.Router();
 
 /* GET home page. */
-router.post("/", async function (req, res, next) {
+router.post("/", function (req, res, next) {
   const payload = req.body;
   console.log(payload);
   try {
-    const response = await axios.request(payload);
-    return res.json(response.data);
+    axios.request(payload);
+    return res.status(200);
   } catch (error) {
     return res.status(500).send(error.message);
   }
